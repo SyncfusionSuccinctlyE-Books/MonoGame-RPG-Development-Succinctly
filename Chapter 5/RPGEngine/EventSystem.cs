56060533a1048cbdfac8ac8700a1c3f669046aa3
Jim Perry <machaira@comcast.net>
2021-10-04 23:18:19 -0400

    Add files via upload

---
+++ Chapter 5/RPGEngine/EventSystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGEngine
+{
+    //Event system could be used for more than quests so events aren't in quest manager
+    //For example, achievements based on obtaining a certain amount of loot
+    //This allow multiple system to use the same events
+
+    public class EventSystemEventArgs
+    {
+        public int ObjectID;    //specific to event, Entity events = entity ID, Item event = item ID, etc.
+        public string Tag;      //could be anything
+    }
+
+    public class EventSystem
+    {
+        public delegate void EventSystemEventHandler(EventSystemEventArgs e);
+
+        public static event EventSystemEventHandler EntityKilled;
+        public static event EventSystemEventHandler LootObtained;
+        public static event EventSystemEventHandler LocationReached;
+        public static event EventSystemEventHandler EntityTalkedTo;
+        public static event EventSystemEventHandler ItemObtained;
+        public static event EventSystemEventHandler LevelEntered;
+        public static event EventSystemEventHandler QuestAssigned;
+
+        public static void OnEntityKilled(EventSystemEventArgs e)
+        {
+            EntityKilled?.Invoke(e);
+        }
+
+        public static void OnLootObtained(EventSystemEventArgs e)
+        {
+            LootObtained?.Invoke(e);
+        }
+
+        public static void OnLocationReached(EventSystemEventArgs e)
+        {
+            LocationReached?.Invoke(e);
+        }
+
+        public static void OnEntityTalkedTo(EventSystemEventArgs e)
+        {
+            EntityTalkedTo?.Invoke(e);
+        }
+
+        public static void OnItemObtained(EventSystemEventArgs e)
+        {
+            ItemObtained?.Invoke(e);
+        }
+
+        public static void OnLevelEntered(EventSystemEventArgs e)
+        {
+            LevelEntered?.Invoke(e);
+        }
+
+        public static void OnQuestAssigned(EventSystemEventArgs e)
+        {
+            QuestAssigned?.Invoke(e);
+        }
+    }
+}
---
+++ Chapter 5/RPGEngine/QuestManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using MonoGameRPG.StateManagement;
+
+namespace RPGEngine
+{
+    public class QuestEventArgs
+    {
+        public string Text;
+
+        public QuestEventArgs(string text)
+        {
+            Text = text;
+        }
+    }
+
+    public class QuestManager
+    {
+        private Character character;
+
+        public delegate void QuestEventHandler(QuestEventArgs e);
+
+        public event QuestEventHandler QuestUpdated;
+
+        public QuestManager(Character character)
+        {
+            EventSystem.EntityKilled += EventSystem_EntityKilled;
+            EventSystem.EntityTalkedTo += EventSystem_EntityTalkedTo;
+            EventSystem.ItemObtained += EventSystem_ItemObtained;
+            EventSystem.LocationReached += EventSystem_LocationReached;
+            EventSystem.LootObtained += EventSystem_LootObtained;
+            EventSystem.LevelEntered += EventSystem_LevelEntered;
+
+            this.character = character;
+        }
+
+        private void EventSystem_LevelEntered(EventSystemEventArgs e)
+        {
+        }
+
+        private void EventSystem_LootObtained(EventSystemEventArgs e)
+        {
+            
+        }
+
+        private void EventSystem_LocationReached(EventSystemEventArgs e)
+        {
+            
+
+        }
+
+        private void EventSystem_ItemObtained(EventSystemEventArgs e)
+        {
+            //Quest type - Item
+            //GetItemID = e.ObjectID
+        }
+
+        private void EventSystem_EntityTalkedTo(EventSystemEventArgs e)
+        {
+            //Quest type - Interact
+            //InteractionType = Talk
+            //StepEntity = e.ObjectID
+
+            //Quest type - Fedex
+            //GiveItemID = e.ObjectID
+
+            foreach(Quest q in character.GetQuests())
+            {
+                AssignedQuest aq = character.AssignedQuests.Find(a => a.QuestID == q.ID);
+                if (aq != null)
+                {
+                    QuestStep step = q.Steps[aq.CurStep];
+                    if (step.Type == QuestStepType.Interact
+                    && step.StepInteractionType == InteractionType.Kill
+                    && step.StepEntity == e.ObjectID)
+                    {
+                        aq.CurStep++;
+                        QuestUpdated(new QuestEventArgs(q.Name));
+                    }
+                }
+            }
+        }
+
+        private void EventSystem_EntityKilled(EventSystemEventArgs e)
+        {
+            //Quest type - Interact
+            //InteractionType = Kill
+            //StepEntity = e.ObjectID
+
+            foreach (Quest q in character.GetQuests())
+            {
+                AssignedQuest aq = character.AssignedQuests.Find(a => a.QuestID == q.ID);
+                if (aq != null)
+                {
+                    aq.NumItemsDone[e.Tag]++;
+
+                    QuestStep step = q.Steps[aq.CurStep];
+
+                    if (step.Type == QuestStepType.Interact
+                        && step.StepInteractionType == InteractionType.Kill
+                        && step.StepEntity == e.ObjectID
+                        && step.Quantity == aq.NumItemsDone[e.Tag])
+                    {
+                        aq.CurStep++;
+                        QuestUpdated(new QuestEventArgs(q.Name));
+
+                    }
+                }
+            }
+        }
+
+        public static string GetQuestName(int id)
+        {
+            return LoadQuest(id).Name;
+        }
+
+        public static Quest LoadQuest(int id)
+        {
+            Quest quest = new Quest();
+
+            string data = File.ReadAllText(@"Content\Data\Quests\" + id.ToString() + ".json");
+
+            quest = JsonConvert.DeserializeObject<Quest>(data);
+
+            return quest;
+        }
+
+        public static QuestRewardType GetQuestReward(int id, out object reward)
+        {
+            Quest quest = QuestManager.LoadQuest(id);
+
+            switch(quest.RewardType)
+            {
+                case QuestRewardType.Money:
+
+                    reward = quest.RewardItemID;
+                    return QuestRewardType.Money;
+
+                case QuestRewardType.Item:
+
+                    //items handled later
+                    reward = quest.RewardItemID;
+                    return QuestRewardType.Item;
+            }
+
+            reward = null;
+            return QuestRewardType.Item;
+        }
+    }
+}
